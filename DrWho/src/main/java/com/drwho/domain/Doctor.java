1151d46b32eebdcbf69c6311af7ab6e54f765ecc
felipe <felipe1okino@live.com>
2017-09-23 03:33:02 -0300

    doctors

---
+++ DrWho/src/main/java/com/drwho/domain/Doctor.java
@@ -0,0 +1,118 @@
+package com.drwho.domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+
+@Entity
+@Table(name = "doctor")
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class Doctor {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String specialization;
+
+
+    public Doctor(){ }
+
+    public Doctor(String name, String cpf, String email, String phoneNumber, String address, String specialization) {
+        setName(name);
+        setCpf(cpf);
+        setEmail(email);
+        setPhoneNumber(phoneNumber);
+        setAddress(address);
+        setSpecialization(specialization);
+    }
+
+    //Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    @Override
+    public String toString(){
+        return "Doctor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNmber='" + phoneNumber + '\'' +
+                ", adreess='" + address +
+                ", specialization='" + specialization + '\'' +
+                '}';
+    }
+
+}
---
+++ DrWho/src/main/java/com/drwho/service/DoctorService.java
@@ -0,0 +1,48 @@
+package com.drwho.service;
+
+import com.drwho.dao.jpa.DoctorRepository;
+import com.drwho.domain.Doctor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorService {
+
+    private static final  Logger log = LoggerFactory.getLogger(DoctorService.class);
+
+    @Autowired
+    private  DoctorRepository doctorRepository;
+
+    @Autowired
+    CounterService counterService;
+
+    @Autowired
+    GaugeService gaugeService;
+
+    public DoctorService(){
+
+    }
+
+    public Doctor createDoctor(Doctor doctor){
+        return doctorRepository.save(doctor);
+    }
+    public Doctor getDoctor(long id) { return doctorRepository.findOne(id); }
+    public void updateDoctor(Doctor doctor){
+        doctorRepository.save(doctor);
+    }
+    public void deleteDoctor(Long id){ doctorRepository.delete(id); }
+
+    public Page<Doctor> getAllDoctors(Integer page, Integer size){
+        Page pageOfDoctors = doctorRepository.findAll(new PageRequest(page, size));
+        if (size > 50) {
+            counterService.increment("DrWho.DoctorService.getAll.largePayload");
+        }
+        return pageOfDoctors;
+    }
+}
